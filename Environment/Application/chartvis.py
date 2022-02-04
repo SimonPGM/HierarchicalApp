d3bccf1d6d73ec1c35637e4946cc769f9d0e8968
SimonPGM <sigaleanom@unal.edu.co>
2022-02-03 22:08:20 -0500

    map remaining

---
+++ Environment/Application/chartvis.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+
+MONTHS = ["Enero", "Febrero", "Marzo", "Abril",
+    "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+    "Octubre", "Noviembre", "Diciembre"
+]
+
+def title_generator(year: int, municiple: str):
+    return f"Comparación entre el modelo y los datos reales en el municipio de {municiple} en el {year}"
+
+def plot(year: int, path: str, municiple: str):
+    
+    municiple = municiple.upper()
+    print(municiple)
+    df = pd.read_csv(path)
+    df = df[(df.Municipio == municiple) & (df.Anio == year)]
+    df.Mes = df.Mes.apply(lambda x: MONTHS[x-1])
+    
+    fig = px.line(df, x="Mes", y="Recuperados", color="Clase",
+    line_dash="Clase", hover_name="Clase", title=title_generator(year, municiple.capitalize()))
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
--- Environment/Application/main.py
+++ Environment/Application/main.py
@@ -1,11 +1,19 @@
 #importing modules
+from crypt import methods
+from cachelib import RedisCache
 from flask import Flask, request, redirect, render_template, session
 from flask_session import Session
+from Application.chartvis import plot
 
 app = Flask(__name__)
 SESSION_TYPE = "filesystem"
 app.config.from_object(__name__)
 Session(app)
+
+def change_chart(year: str, city:str):
+
+    session['dates_chart']["year"] = int(year)
+    session['dates_chart']["city"] = city
 
 #Main
 @app.route("/")
@@ -13,11 +21,13 @@ def index():
     #Creating session variables for the user
     if 'dates_chart' not in session:
         session['dates_chart'] = {
-        "date": {"year": 2020, "month": 3},
+        "year": 2020,
+        "city": "Medellin"
         }
     if 'dates_map' not in session:
         session['dates_map'] = {
-        "date": {"year": 2020, "month": 3},
+        "year": 2020,
+        "month": 3
         }
     
     return render_template("index.html")
@@ -27,10 +37,18 @@ def description():
     
     return render_template("index.html")
 
-@app.route("/model/chart")
+@app.route("/model/chart", methods=["GET", "POST"])
 def model_chart():
-    
-    return render_template("index.html")
+
+    if request.method == "POST":
+        year = request.form["chartyear"]
+        city = request.form["chartcity"]
+        change_chart(year, city)
+        return redirect("/model/chart")
+        
+    return render_template("chart.html",
+    graphJSON=plot(session['dates_chart']['year'], "Application/Results.csv",
+    session['dates_chart']["city"]))
 
 @app.route("/model/map")
 def model_map():
